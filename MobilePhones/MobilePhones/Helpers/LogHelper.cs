c9f21b64ea61ec4ce8ac9cd0fd0bbb236ba1ff2c
DESKTOP-V0IKCBA\Otari <otarikharaishvili@gmail.com>
2018-10-25 19:16:06 +0400

    LogHelper created

---
+++ MobilePhones/MobilePhones/Helpers/LogHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MobilePhones.Helpers
+{
+    public class LogHelper
+    {
+        public static void LogError(Exception e)
+        {
+            File.AppendAllText(HttpContext.Current.Server.MapPath(@"\App_Data\Errors.txt"), e.Message + "\n\n");
+        }
+    }
+}
